acfe755fbcb08df1e02b94d75f8872e5e07e8778
Ramon Nunes <ramonjs.dev@gmail.com>
2022-01-26 16:53:13 -0300

    Rota com um vídeo

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ app.get('/stream', (req, res) => {
   const songSize = fs.statSync(songPath).size
 
   const start = Number(range?.replace(/\D/g, ''))
-  const CHUNK_SIZE = 1000
+  const CHUNK_SIZE = 3000
   const end = Math.min(start + CHUNK_SIZE, songSize - 1)
   const headers = {
     'Content-Range': `bytes ${start}-${end}/${songSize}`,
@@ -25,6 +25,26 @@ app.get('/stream', (req, res) => {
   res.writeHead(206, headers)
 
   const songStream = fs.createReadStream(songPath, { start, end })
+  return songStream.pipe(res)
+})
+
+app.get('/video-stream', (req, res) => {
+  const range = req.headers.range
+  const videoPath = resolve(__dirname, 'videos', 'Cavaleiro-da-Lua_Marvel-Studios_Trailer-Oficial-Dublado_Disney+-(1080p).mp4')
+  const videoSize = fs.statSync(videoPath).size
+
+  const start = Number(range?.replace(/\D/g, ''))
+  const CHUNK_SIZE = 10000
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Rangers': `bytes`,
+    'Content-Type': 'video/mp4'
+  }
+
+  res.writeHead(206, headers)
+
+  const songStream = fs.createReadStream(videoPath, { start, end })
   return songStream.pipe(res)
 })
 
